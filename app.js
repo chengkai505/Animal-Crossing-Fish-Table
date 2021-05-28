e6984b2f671af3ae1b368fda398295ea1b4da3a4
chengkai505 <wuvincentck505@gmail.com>
2021-05-28 21:53:09 +0800

    renew

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-﻿'use strict';
+'use strict';
 if (location.hash.substr(1).length == Math.ceil(document.getElementsByClassName("checkbox").length / 5)) {
     if (!decode()) {
         alert("解析失敗");
